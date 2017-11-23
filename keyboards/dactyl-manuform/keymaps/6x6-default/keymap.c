6570958b35a0dedf52ec1f67a6d2a3564232417a
matt1003 <matt1003@gmail.com>
2017-11-24 12:33:35 +1300

    swap up/down keys to match vim

--- keyboards/dactyl-manuform/keymaps/6x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/6x6-default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_Y,      KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS   },
     { KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT   },
     { KC_N,      KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSHIFT },
-    { XXXXX,     KC_UP,    KC_DOWN,  KC_LBRC, KC_RBRC,   MO(_FN)   },
+    { XXXXX,     KC_DOWN,  KC_UP,    KC_LBRC, KC_RBRC,   MO(_FN)   },
     { KC_RCTRL,  KC_PGUP,  KC_SPC,   KC_ENTER, KC_PGDN,  KC_RALT   },
   },
   [_FN] = {
