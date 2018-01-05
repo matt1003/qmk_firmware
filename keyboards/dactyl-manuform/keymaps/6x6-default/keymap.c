5593c68410881e7bd99dd1974cd7968d586dccc2
matt1003 <matt1003@gmail.com>
2018-01-05 20:52:11 +1300

    fix white space

--- keyboards/dactyl-manuform/keymaps/6x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/6x6-default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_Y,      KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS   },
     { KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT   },
     { KC_N,      KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSHIFT },
-    { XXXXX,     KC_DOWN,  KC_UP,    KC_LBRC, KC_RBRC,   MO(_SYM)  },
+    { XXXXX,     KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,  MO(_SYM)  },
     { KC_RCTRL,  KC_PGUP,  KC_SPC,   KC_ENTER, KC_PGDN,  KC_RALT   },
   },
   [_SYM] = {
