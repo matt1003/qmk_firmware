2322a83109e033fe203dfd5ed407c5bcc8345713
matt1003 <matt1003@gmail.com>
2018-02-03 16:59:23 +1300

    fix build

--- keyboards/dactyl/keymaps/micro/keymap.c
+++ keyboards/dactyl/keymaps/micro/keymap.c
@@ -121,7 +121,7 @@ void select_modifier(keyrecord_t *record, uint16_t modifier, uint16_t keycode)
         else
         {
             if (modifier == KC_LSFT || modifier == KC_RSFT)
-                shifted = true;
+                shift = true;
             register_code(modifier);
         }
     }
@@ -151,16 +151,16 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     switch (id)
     {
         case LOWER:
-            select_layer(record, _RAISE, KC_TAB);
+            select_layer(record, _LOWER, KC_BSPC);
             break;
         case RAISE:
-            select_layer(record, _LOWER, KC_BSPC);
+            select_layer(record, _RAISE, KC_TAB);
             break;
         case CNRTL:
             select_modifier(record, KC_RCTL, KC_SPC);
             break;
         case SHIFT:
-            switch_modifier(record, KC_LSFT, KC_ENT);
+            select_modifier(record, KC_LSFT, KC_ENT);
             break;
     }
     return MACRO_NONE;
@@ -182,12 +182,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = {
         /* left hand */
         { _______, _______, _______, _______, _______, _______ },
-        { _______,  C_Q,    KC_W,    KC_E,    KC_R,    KC_T    },
+        { _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T    },
         { KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G    },
         { _______,LSFT_T(KC_Z),KC_X, KC_C,    KC_V,    KC_B    },
         { _______, _______, _______, _______, _______, XXXXXXX },
         /* left thumb */
-        { _______, LALT_T(KC_DEL), F(LOWER), F(CNRTL), _______, _______ },
+        { RESET, LALT_T(KC_DEL), F(LOWER), F(CNRTL), KC_NO, TG(_COLEMAK)},
         /* right hand */
         { _______, _______, _______, _______, _______, _______ },
         { KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______ },
@@ -195,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         { KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______ },
         { XXXXXXX, _______, _______, _______, _______, _______ },
         /* right thumb */
-        { _______, _______, F(SHIFT), F(RAISE), RGUI_T(KC_ESC), _______ },
+        { TG(_COLEMAK), KC_NO, F(SHIFT), F(RAISE), RGUI_T(KC_ESC), RESET },
     },
     [_COLEMAK] = {
         /* left hand */
@@ -205,7 +205,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         { _______,LSFT_T(KC_Z),KC_X, KC_C,    KC_V,    KC_B    },
         { _______, _______, _______, _______, _______, XXXXXXX },
         /* left thumb */
-        { _______, LALT_T(KC_DEL), F(LOWER), F(CNRTL), _______, _______ },
+        { _______, _______, _______, _______, _______, _______ },
         /* right hand */
         { _______, _______, _______, _______, _______, _______ },
         { KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______ },
@@ -213,58 +213,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         { KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______ },
         { XXXXXXX, _______, _______, _______, _______, _______ },
         /* right thumb */
-        { _______, _______, F(SHIFT), F(RAISE), RGUI_T(KC_ESC), _______ },
+        { _______, _______, _______, _______, _______, _______ },
     },
     [_LOWER] = {
         /* left hand */
         { _______, _______, _______, _______, _______, _______ },
-        { _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______ },
-        { _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______ },
-        { _______, KC_TILD, KC_PLUS, KC_MINS, KC_EQL,  _______ },
+        { _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_NO   },
+        { _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_NO   },
+        { _______, KC_TILD, KC_PLUS, KC_MINS, KC_EQL,  KC_NO   },
         { _______, _______, _______, _______, _______, XXXXXXX },
         /* left thumb */
-        { RESET,   _______, _______, _______, _______, TG(_COLEMAK) },
+        { _______, _______, _______, _______, _______, _______ },
         /* right hand */
         { _______, _______, _______, _______, _______, _______ },
-        { _______, KC_7,    KC_8,    KC_9,    KC_COMM, _______ },
-        { _______, KC_4,    KC_5,    KC_6,    KC_0,    _______ },
-        { _______, KC_1,    KC_2,    KC_3,    KC_DOT,  _______ },
+        { KC_NO,   KC_7,    KC_8,    KC_9,    KC_COMM, _______ },
+        { KC_NO,   KC_4,    KC_5,    KC_6,    KC_0,    _______ },
+        { KC_NO,   KC_1,    KC_2,    KC_3,    KC_DOT,  _______ },
         { XXXXXXX, _______, _______, _______, _______, _______ },
         /* rght thumb */
-        { TG(_COLEMAK), _______, _______, _______, _______, RESET },
+        { _______, _______, _______, _______, _______, _______ },
     },
     [_RAISE] = {
         /* left hand */
         { _______, _______, _______, _______, _______, _______ },
-        { _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______ },
-        { _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______ },
-        { _______, KC_BSLS, KC_PIPE, KC_QUOT, KC_DQUO, _______ },
+        { _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_NO   },
+        { _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO   },
+        { _______, KC_BSLS, KC_PIPE, KC_QUOT, KC_DQUO, KC_NO   },
         { _______, _______, _______, _______, _______, XXXXXXX },
         /* left thumb */
-        { RESET,   _______, _______, _______, _______, TG(_COLEMAK) },
+        { _______, _______, _______, _______, _______, _______ },
         /* right hand */
         { _______, _______, _______, _______, _______, _______ },
-        { _______, _______, KC_LCBR, KC_RCBR, KC_GRV,  _______ },
-        { _______, _______, KC_LPRN, KC_RPRN, KC_DREF, _______ },
-        { _______, _______, KC_LBRC, KC_RBRC, KC_HDIR, _______ }
+        { KC_NO,   KC_NO,   KC_LCBR, KC_RCBR, KC_GRV,  _______ },
+        { KC_NO,   KC_NO,   KC_LPRN, KC_RPRN, KC_DREF, _______ },
+        { KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_HDIR, _______ },
         { XXXXXXX, _______, _______, _______, _______, _______ },
         /* rght thumb */
-        { TG(_COLEMAK), _______, _______, _______, _______, RESET },
+        { _______, _______, _______, _______, _______, _______ },
     },
     [_ADJUST] = {
         /* left hand */
         { _______, _______, _______, _______, _______, _______ },
-        { _______, KC_APP,  KC_PAUS, KC_INS,  KC_PSCR, _______ },
-        { _______, KC_CAPS, KC_NLCK, KC_SLCK, KC_LALT, _______ },
-        { _______, KC_EQEQ, KC_LTEQ, KC_GTEQ, KC_NTEQ, _______ },
+        { _______, KC_APP,  KC_PAUS, KC_INS,  KC_PSCR, KC_NO   },
+        { _______, KC_CAPS, KC_NLCK, KC_SLCK, KC_LALT, KC_NO   },
+        { _______, KC_EQEQ, KC_LTEQ, KC_GTEQ, KC_NTEQ, KC_NO   },
         { _______, _______, _______, _______, _______, XXXXXXX },
         /* left thumb */
         { _______, _______, _______, _______, _______, _______ },
         /* right hand */
         { _______, _______, _______, _______, _______, _______ },
-        { _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______ },
-        { _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______ },
-        { _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______ },
+        { KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______ },
+        { KC_NO,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______ },
+        { KC_NO,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______ },
         { XXXXXXX, _______, _______, _______, _______, _______ },
         /* rght thumb */
         { _______, _______, _______, _______, _______, _______ },
